20f3c88b5efefe870ab8e21cfa35cab8aa98016a
menachem margolis <margolismendel5728@gmail.com>
2021-01-11 18:56:04 -0500

    commit tree 3eab9ea73893aa84512fc88b5a24831edc7730f2

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+    "Hello, World!" 
+    end
+
+    get '/hometown' do
+        "My hometown is montreal"
+    end
+
+    get '/name' do
+        "My name is mendel "
+    end
+
+    get '/favorite-song' do
+       " My favorite song is  amazing grace "
+    end
 end
